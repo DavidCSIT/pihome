6713e093be8566da38bffff4e75def79f3b4d71c
David Cartman <david.cartman@sit.ac.nz>
2021-11-22 16:12:30 +1300

    watering controller v1

--- wateringcontroller.py
+++ wateringcontroller.py
@@ -1,74 +1,16 @@
 #!/usr/bin/env python3
-from time import sleep
-from datetime import datetime
-import RPi.GPIO as GPIO
-import sys
-import os
-import config
-import pymysql
+import rainmaker    #supporting functions
+import config       #import configuration variables
 
-def main():
+def water_if_required() :
+    #if argument is provided water now
+    if len(sys.argv) > 1:
+        rainmaker.log_and_notify(f"Watering Started Manual watering")
+        rainmaker.open_valve(config.WATERING_TIME)
+        quit()
+    
+    #get latest forecast
+    #forecasts = rainmaker.get_forecast()
    
- # Connect to the database.
-    conn = pymysql.connect(db=DATABASE,
-        user=USER,
-        passwd=PASSWD,
-        host=HOST)
-    c = conn.cursor()
-        
-    relay = GroveRelay(5)
+water_if_required()
 
-    while True:
-        now = datetime.now()
-        log_time = datetime.now().strftime("%H.%M.%S")
-
-        c.execute(f"SELECT value from config where id='temp';")
-        data = c.fetchone()
-        temp = int(data[0])
-
-        c.execute(f"SELECT value from config where id='mintemp';")
-        data = c.fetchone()
-        mintemp = int(data[0])
-
-        c.execute(f"SELECT value from config where id='humidity';")
-        data = c.fetchone()
-        humidity = int(data[0])
- 
-        c.execute(f"SELECT value from config where id='override';")
-        data = c.fetchone()
-        override = int(data[0])  
-        
-        values = f"temp {temp}{DEGREE_SIGN} mintemp {mintemp}{DEGREE_SIGN} humidity {humidity}%"
-       
-        if (override != 0 ):
-            c.execute(f"UPDATE config set value='0' where id='override';")
-            c.execute(f"UPDATE config set value='T' where id='heating';")
-            conn.commit()
-            log(f"Manual {override} hour {values}")
-            relay.on()
-            sleep(override*60*60)
-            relay.off()
-            c.execute(f"UPDATE config set value='F' where id='heating';")
-            conn.commit()
-        elif (temp <= mintemp):
-            relay.on()
-            c.execute(f"UPDATE config set value='T' where id='heating';")
-            conn.commit()
-            log(f"Below min temp Heating on {values}")
-            sleep(MIN_RUN_TIME)       
-        else:
-            c.execute(f"UPDATE config set value='F' where id='heating';")
-            conn.commit()
-            relay.off()
-            log(f"All G temperature {values}")
-            sleep(60)
-   
-def log(message, important=False):
-       
-    txt = open(f'{HOME}log.txt', "a")
-    log_time = datetime.now().strftime("%H.%M.%S")
-    message = f"{log_time} {message}"
-    print(message)
-    txt.write(f"{message} \n")
-   
-main()
